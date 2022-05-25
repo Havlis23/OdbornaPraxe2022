996ad75eeea8ea4312c2b63ed7b65f2aea431d40
Matyas Krecek <59233911+matyas97@users.noreply.github.com>
2022-05-25 15:27:40 +0200

    hints #1

--- main.js
+++ main.js
@@ -4,54 +4,52 @@ const email = document.getElementById('email');
 const phone = document.getElementById('tel.cislo');
 const company = document.getElementById('spolecnost');
 
-form.addEventListener('submit', e => {
-	e.preventDefault();
-	
-	checkInputs();
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(e);
+
+    checkInputs();
 });
 
 function checkInputs() {
-	const usernameValue = username.value.trim();
-	const emailValue = email.value.trim();
-	const phoneValue = phone.value.trim();
-	const companyValue = company.value.trim();
-	
-	if(usernameValue === '') {
-		setErrorFor(username, 'Jméno nesmí být prázdné');
-	} else {
-		setSuccessFor(username);
-	}
-	
-	if(emailValue === '') {
-		setErrorFor(email, 'Zadejte správný Email');
-	} else if (!isEmail(emailValue)) {
-		setErrorFor(email, 'Zadali jste neplatný Email');
-	} else {
-		setSuccessFor(email);
-	}
-	
-	if(phoneValue === '') {
-		setErrorFor(phone, 'Telefonní číslo nesmí být prázdné');
-	} else {
-		setSuccessFor(phone);
-	}
-	
-	if(companyValue === '') {
-		setErrorFor(company, 'Toto políčko je poovinné');
-	}
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const phoneValue = phone.value.trim();
+    const companyValue = company.value.trim();
+
+    if (usernameValue === '') {
+        setErrorFor(username, 'Jméno nesmí být prázdné');
+    } else {
+        setSuccessFor(username);
+    }
+
+    if (emailValue === '') {
+        setErrorFor(email, 'Zadejte správný Email');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Zadali jste neplatný Email');
+    } else {
+        setSuccessFor(email);
+    }
+
+    if (phoneValue === '') {
+        setErrorFor(phone, 'Telefonní číslo nesmí být prázdné');
+    } else {
+        setSuccessFor(phone);
+    }
+
+    if (companyValue === '') {
+        setErrorFor(company, 'Toto políčko je poovinné');
+    }
 }
 
 function setErrorFor(input, message) {
-	const formControl = input.parentElement;
-	const small = formControl.querySelector('small');
-	formControl.className = 'form-control error';
-	small.innerText = message;
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'form-control error';
+    small.innerText = message;
 }
 
 function setSuccessFor(input) {
-	const formControl = input.parentElement;
-	formControl.className = 'form-control success';
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
 }
-
-
-
