1da7bbdf2910b61ee2bcf312792432e349fe458b
Havlis23 <davidek.havel@gmail.com>
2022-05-27 12:18:54 +0200

    3. publikace

---
+++ script.js
@@ -0,0 +1,8 @@
+//const onSubmit = () => {
+//    const dataToSend = document.getElementById("form").innerText
+//
+//    if(dataToSend) {
+//        fetch("https://hook.eu1.make.com/skjhpwyxamnb1nvmgmprkrcvrgzi4xyb", {method: "POST", body: dataToSend})
+//        window.location.href = "thankyoupage.html"
+//    }
+//}
--- script.ts
+++
@@ -1,8 +0,0 @@
-const onSubmit = () => {
-    const dataToSend = document.getElementById("form")?.innerText
-
-    if(dataToSend) {
-        fetch("https://hook.eu1.make.com/skjhpwyxamnb1nvmgmprkrcvrgzi4xyb", {method: "POST", body: dataToSend})
-        window.location.href = "thankyoupage.html"
-    }
-}
